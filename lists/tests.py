301371fc6fb136e7f5a1223c907291e21179da19
sergio <khotsenkos1993k@gmail.com>
2022-11-17 15:38:52 +0200

    Remove unnecessary tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,4 @@
-from django.http import HttpRequest
-from django.urls import resolve
 from django.test import TestCase
-from lists.views import home_page
-from django.template.loader import render_to_string
 
 from lists.models import Item
 
@@ -56,17 +52,3 @@ class HomePageTest(TestCase):
 
         self.assertIn('itemy 1', response.content.decode())
         self.assertIn('itemy 2', response.content.decode())
-
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
-        self.assertTemplateUsed(response, 'home.html')
