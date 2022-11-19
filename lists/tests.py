216e51c5a9399ac97295a3824c9f14f97398e237
sergio <khotsenkos1993k@gmail.com>
2022-11-19 14:21:29 +0200

    New url, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -3,25 +3,6 @@ from django.test import TestCase
 from lists.models import Item
 
 
-class ItemModelTest(TestCase):
-    def test_save_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.save()
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_item_saved = saved_items[0]
-        second_item_saved = saved_items[1]
-        self.assertEqual(first_item_saved.text, 'The first (ever) list item')
-        self.assertEqual(second_item_saved.text, 'Item the second')
-
-
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
@@ -42,13 +23,38 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemy 1')
-        Item.objects.create(text='itemy 2')
 
-        response = self.client.get('/')
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
 
-        self.assertIn('itemy 1', response.content.decode())
-        self.assertIn('itemy 2', response.content.decode())
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+
+class ItemModelTest(TestCase):
+    def test_save_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_item_saved = saved_items[0]
+        second_item_saved = saved_items[1]
+        self.assertEqual(first_item_saved.text, 'The first (ever) list item')
+        self.assertEqual(second_item_saved.text, 'Item the second')
