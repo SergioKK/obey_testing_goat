8d7d2aa4e7a712a913c66c6147dbfc632e30983b
sergio <khotsenkos1993k@gmail.com>
2022-11-29 13:31:23 +0200

    Fix bug in functional test list item validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -13,12 +13,6 @@ class ItemValidationTest(FunctionalTest):
         # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.live_server_url)
         self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
-        # The home page refreshes, and there is an error message saying
-        # that list items cannot be blank
-        self.assertEqual(
-            self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
-            "You can't have an empty list item"
-        )
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
