942429cb0264d5a8c5a1b583e949c04b28a75c5a
sergio <khotsenkos1993k@gmail.com>
2022-12-05 11:34:59 +0200

    Add new validation logic

--- lists/forms.py
+++ lists/forms.py
@@ -8,10 +8,9 @@ DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 
 class ItemForm(forms.models.ModelForm):
-
     class Meta:
         model = Item
-        fields = ('text', )
+        fields = ('text',)
         widgets = {
             'text': forms.fields.TextInput(attrs={
                 'placeholder': 'Enter a to-do item',
@@ -31,6 +30,7 @@ class ExistingListItemForm(ItemForm):
 
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.instance.list = for_list
 
     def validate_unique(self):
         try:
@@ -38,3 +38,6 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
